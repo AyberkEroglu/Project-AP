175ab5d82695158fbf9baba3b5b1fc63d2f543f6
Ayberk Berat Eroğlu <ayberkberateroglu@gmail.com>
2022-05-09 16:05:54 +0300

    Created the LIBGDX project file

---
+++ Project AP/desktop/src/com/mygdx/projectap/DesktopLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.projectap;
+
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.projectap.ProjectAP;
+
+import java.awt.*;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		//config.setTitle("Project AP");
+		//config.useVsync(true);
+		new Lwjgl3Application(new ProjectAP(), config);
+	}
+}
