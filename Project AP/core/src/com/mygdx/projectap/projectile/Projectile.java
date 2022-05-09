a0f315ae260caa2ba9ff9b4ca1b7d3bb2d9f63e5
Ayberk Berat Eroğlu <ayberkberateroglu@gmail.com>
2022-05-09 16:59:35 +0300

    Additional changes on Projectile and RangedWeapon

--- Project AP/core/src/com/mygdx/projectap/projectile/Projectile.java
+++ Project AP/core/src/com/mygdx/projectap/projectile/Projectile.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Projectile {
 
-    public static final Projectile BULLET = new Bullet();
+    public static final Projectile BULLET = new Projectile(new Texture("kenneyindustrialpack/kenney_platformerpack_industrial/PNG/Default size/platformIndustrial_009.png"), 200, 1);
     protected float damage;
     protected float velocity;
     protected Vector2 direction;
--- Project AP/core/src/com/mygdx/projectap/weapon/RangedWeapon.java
+++ Project AP/core/src/com/mygdx/projectap/weapon/RangedWeapon.java
@@ -1,13 +1,31 @@
 package com.mygdx.projectap.weapon;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.projectap.projectile.Projectile;
 
 
 public class RangedWeapon extends Weapon{
-    protected Projectile[] projectiles;
+    protected Array<Projectile> projectiles;
+    protected Vector2 direction;
+    protected int projectileCount;
 
-    public RangedWeapon(int range, Texture texture) {
+    public RangedWeapon(int range, Texture texture, int projectileCount) {
+        this(range,texture,projectileCount, Projectile.BULLET);
+
+    }
+
+    public RangedWeapon(int range, Texture texture, int projectileCount, Projectile projectileType) {
         super(range, texture);
+        this.projectileCount = projectileCount;
+        createProjectiles(projectileCount,projectileType);
+    }
+
+    private void createProjectiles(int projectileCount, Projectile projectileType) {
+        projectiles = new Array<>();
+        for (int i = 0; i < projectileCount; i++) {
+            projectiles.add(projectileType);
+        }
     }
 
     @Override
