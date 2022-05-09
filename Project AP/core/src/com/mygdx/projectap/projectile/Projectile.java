b5020e1b9e2fd953cddbac1fe4256ff26e504e62
Osmanb <osmanberkayartac@gmail.com>
2022-05-09 16:55:15 +0300

    Projectile and bullet classes and constructors

--- Project AP/core/src/com/mygdx/projectap/projectile/Projectile.java
+++ Project AP/core/src/com/mygdx/projectap/projectile/Projectile.java
@@ -1,5 +1,23 @@
 package com.mygdx.projectap.projectile;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
 public class Projectile {
+
+    public static final Projectile BULLET = new Bullet();
+    protected float damage;
+    protected float velocity;
+    protected Vector2 direction;
+    protected Texture texture;
+
+
+    public Projectile(Texture texture, float velocity, float damage) {
+
+
+    }
+
+
+
 
 }
