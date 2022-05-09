c646f7065389e1a5ea9f940f3ed027a499d41764
Ayberk Berat Eroğlu <ayberkberateroglu@gmail.com>
2022-05-09 16:22:19 +0300

    Created the package of entities

--- Project AP/desktop/src/com/mygdx/projectap/DesktopLauncher.java
+++ Project AP/desktop/src/com/mygdx/projectap/DesktopLauncher.java
@@ -12,8 +12,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		//config.setTitle("Project AP");
-		//config.useVsync(true);
+		config.useVsync(true);
 		new Lwjgl3Application(new ProjectAP(), config);
 	}
 }
