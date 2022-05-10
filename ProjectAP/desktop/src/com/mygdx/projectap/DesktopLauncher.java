57fe0916b32301f37e48deac05e4c4dd0ac50364
Ayberk Berat Eroğlu <ayberkberateroglu@gmail.com>
2022-05-10 17:05:57 +0300

    setResizable(false)

--- ProjectAP/desktop/src/com/mygdx/projectap/DesktopLauncher.java
+++ ProjectAP/desktop/src/com/mygdx/projectap/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setWindowedMode(ProjectAP.WIDTH,ProjectAP.HEIGHT);
+		config.setResizable(false);
 		new Lwjgl3Application(new ProjectAP(), config);
 	}
 }
