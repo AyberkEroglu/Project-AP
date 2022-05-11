9002529331d76c63dc5af037ac3e9efad2526a51
Orhun Aysan <orhunaysan3@gmail.com>
2022-05-11 09:00:53 +0300

    Map adjustments.

---
+++ ProjectAP/assets/maps/map3/Background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Background" tilewidth="70" tileheight="70" tilecount="792" columns="36">
+ <image source="Background.jpg" width="2560" height="1600"/>
+</tileset>
